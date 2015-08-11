bec5eaba8569dcc42331ed3927ae9283c3444381
key <657359703@qq.com>
2015-08-11 21:35:22 +0800

    mvc-web

---
+++ mvc-web/src/main/java/com/key/mvcweb/RestConstroller.java
@@ -0,0 +1,30 @@
+package com.key.mvcweb;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RestConstroller {
+	
+	public RestConstroller() {
+	}
+
+	@RequestMapping(value = "/login/{user}", method = RequestMethod.GET)
+	public ModelAndView myMethod(HttpServletRequest request,
+			HttpServletResponse response, @PathVariable("user") String user,
+			ModelMap modelMap) throws Exception {
+		modelMap.put("loginUser", user);
+		return new ModelAndView("/login/hello", modelMap);
+	}
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String registPost() {
+		return "/welcome";
+	}
+}
